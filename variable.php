745e1dd9a6eef3bce21b15b9f1be8e4cbbfaf92a
Hansani Gunathilaka <hansinima20003@gmail.com>
2023-07-10 19:03:06 +0530

    updated

--- variable.php
+++ variable.php
@@ -12,6 +12,12 @@
         <input type="text" name="username">
         <button type = "submit">Submit</button>
     </form>
+
+    <!-- Can't use in here. because username variable is declared below this code segment. -->
+     <!-- <h1> 
+        <?php echo $username ?>  
+    </h1>  -->
+    
 <?php
 
     //$myname = "Hansani"; //initialize a variable
@@ -21,6 +27,18 @@
     echo $username." is my name";
 
 ?>
+
+    <h1>
+        <?php echo $username ?>
+    </h1>
+
+    <p>
+        <?php echo $username ?>
+    </p>
+
+    <a href="#">
+        <?php echo $username ?>
+    </a>
 
 
 </body>
