351d1537005431daf61fa2cdc51bd169b3313609
Hansani Gunathilaka <hansinima20003@gmail.com>
2023-07-10 19:08:48 +0530

    updated

---
+++ sum-of-two-strings.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>sum-string</title>
+</head>
+<body>
+    
+
+    <?php
+
+        $var1 = "Hello "; 
+        $var2 = "World";
+        $var3 = $var1. $var2; //sum of two strings. Use '.' to concatenate two srings.
+
+        echo $var3; //printing concatenated string
+    ?>
+
+</body>
+</html>
